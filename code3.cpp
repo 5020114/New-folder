518925804ce334ce173a94b62f9f85b995a86ebb
5020114 <5020114@fcrit.onmicrosoft.com>
2022-07-31 17:49:48 +0530

    modified:   code3.cpp

--- code3.cpp
+++ code3.cpp
@@ -7,17 +7,5 @@ int main()
     while(t--)
     {
 
-        int word[n],i;
-        for(i=0;i<n;i++)
-            cin>>word[i];
-        int m;
-        cin>>m;
-        int keys[m];
-        for(i=0;i<m;i++)
-            cin>>keys[i];
-        for(i=0;i<n;i++)
-        {
-            
-        }    
     }
 }
