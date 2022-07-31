24b4d51060ed048f589b0f4e3bccb184450a9f36
5020114 <5020114@fcrit.onmicrosoft.com>
2022-07-31 17:43:46 +0530

    new file:   .vscode/settings.json 	new file:   .vscode/tasks.json 	new file:   Solution(1).cpp 	new file:   Solution(2).cpp 	new file:   Solution.cpp 	new file:   code.cpp 	new file:   code2.cpp 	new file:   code3.cpp 	new file:   dragon.cpp 	new file:   dragon.exe 	new file:   new.cpp 	new file:   new.html 	new file:   new.py 	new file:   newClass.java

---
+++ new.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace  std;
+int main()
+{
+    for ( j = 0; j < 5; j++)
+    {
+        cout<<"Hello";
+    }
+    while (j!=0)
+    {
+        cout<<"World";
+        j--;
+    }
+    if ()
+    {
+        cout<<"Abc";
+    }
+    if (/* condition */)
+    {
+        /* code */
+    }
+    else
+    {
+        /* code */
+    }
+    
+    
+    
+    
+}
